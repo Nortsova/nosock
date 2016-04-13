84b8a21e88fda54f8e38e9f2d35cf747a20c723d
Nortsova <anastasi.smch@gmail.com>
2016-04-13 19:25:09 +0300

    1

---
+++ dev/temp/modulesData.js
@@ -0,0 +1,50 @@
+/* Module data structure */
+
+// moduleName: {
+//     dataType: {
+//         property: value
+//     }
+// }
+
+/* Module data example */
+
+_template: {
+    big: {
+        title: 'Hello world',
+        age: 10,
+        button: false
+    }
+},
+
+head: {
+    defaults: {
+        title: 'default title',
+        useSocialMetaTags: true
+    }
+},
+
+__pages: [{
+                name: 'cart',
+                href: 'cart.html'
+             },{
+                name: 'chekout',
+                href: 'chekout.html'
+             },{
+                name: 'home-page',
+                href: 'home-page.html'
+             },{
+                name: 'index',
+                href: 'index.html'
+             },{
+                name: 'product-page',
+                href: 'product-page.html'
+             },{
+                name: 'product',
+                href: 'product.html'
+             },{
+                name: 'sitemap',
+                href: 'sitemap.html'
+             },{
+                name: 'wish-list',
+                href: 'wish-list.html'
+             }]
